746c4fe581926c132e1986657c9fd08e5dd2f5c9
SONY <SONY@SONY-VAIO>
2018-10-16 05:48:06 +0530

    added excel package

---
+++ TestNGTutorial/src/ExcelTutorial/fileInputStreamUsage.java
@@ -0,0 +1,15 @@
+
+
+package ExcelTutorial;
+
+import java.io.FileInputStream;
+
+public class fileInputStreamUsage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//FileInputStream fx = new FileInputStream();
+	}
+
+}
